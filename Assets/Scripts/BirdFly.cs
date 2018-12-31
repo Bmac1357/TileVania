9d41164943174f1a987cdb887947aaf783a759d0
bmac1357 <bmac1357@gmail.com>
2018-12-31 14:37:05 +0000

    Add some extra flavour with animated sprites.

---
+++ Assets/Scripts/BirdFly.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdFly : MonoBehaviour {
+
+
+    [SerializeField] float flySpeed;
+    [SerializeField] float xLimit;
+
+    Vector3 startPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(new Vector3(Vector2.right.x * flySpeed * Time.deltaTime, 0f, 0f));	
+
+        if (transform.position.x > xLimit)
+        {
+            transform.position = startPos;
+        }
+	}
+}
